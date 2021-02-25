3f01ed7f508ce1cccebd7a8ed3a01608caeebf2d
Westindiess <gregoryalexiss@gmail.com>
2021-02-25 17:37:26 +0100

    Exo expressions conditions JS

---
+++ Vote.js
@@ -0,0 +1,5 @@
+let age = 17
+let canVote = true
+
+let msg = canVote = age >= 18 ? 'You can vote' : 'You cannot vote'
+console.log(msg)
---
+++ Welcome.js
@@ -0,0 +1,6 @@
+let firstName = 'Alice'
+let gender = 'female'
+let msg = gender === 'male' ? `Bonjour, ${firstName}, vous êtes entré sur le chan` : `Bonjour, ${firstName} vous êtes entrée sur le chan`
+
+
+console.log(msg)
